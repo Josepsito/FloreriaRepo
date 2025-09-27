014bfedbe152edfadf61b241311bbb37d1c8d453
JosephTocre <u22304704@utp.edu.pe>
2025-09-27 05:54:09 -0500

    Agregando contacto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { Catalogo } from './catalogo/catalogo';
 import { Blog } from './blog/blog';
 import { Novias } from './novias/novias';
+import { Contacto } from './contacto/contacto';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'catalogo', component: Catalogo },
   { path: 'blog', component: Blog },
-  { path: 'novias', component: Novias }
+  { path: 'novias', component: Novias },
+  { path: 'contacto', component: Contacto }
 ];
