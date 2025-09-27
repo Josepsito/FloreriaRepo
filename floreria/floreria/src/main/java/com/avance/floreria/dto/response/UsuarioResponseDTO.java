0f1d2e2b17664f50df6bc7c57cc05be8c3de6362
JosephTocre <u22304704@utp.edu.pe>
2025-09-26 23:24:27 -0500

    Cambios de archivos

--- floreria/floreria/src/main/java/com/avance/floreria/dto/response/UsuarioResponseDTO.java
+++
@@ -1,9 +0,0 @@
-package com.avance.floreria.dto.response;
-
-public record UsuarioResponseDTO(
-        String nombre,
-        String email,
-        String telefono,
-        String direccion
-) {
-}
--- floreria/floreria/src/main/java/com/avance/floreria/service/CarritoService.java
+++
@@ -1,7 +0,0 @@
-package com.avance.floreria.service;
-
-import com.avance.floreria.dto.response.CarritoResponseDTO;
-
-public interface CarritoService {
-    CarritoResponseDTO findById(int id);
-}
