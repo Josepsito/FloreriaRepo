a92624675ff4f9a255d5fa33077a80233cf7702c
AndreaMelisa <U22300170@utp.edu.pe>
2025-09-27 05:33:57 -0500

    Agregando novias

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
 import { Catalogo } from './catalogo/catalogo';
+import { Blog } from './blog/blog';
+import { Novias } from './novias/novias';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'catalogo', component: Catalogo }
+  { path: 'catalogo', component: Catalogo },
+  { path: 'blog', component: Blog },
+  { path: 'novias', component: Novias }
 ];
---
+++ frontend/src/app/novias/novias.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-novias',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './novias.html',
+  styleUrl: './novias.css'
+})
+export class Novias { }
+
