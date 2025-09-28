e00e71271cb3dcdb67c68f1175618ea7186762b7
JosephTocre <u22304704@utp.edu.pe>
2025-09-28 12:22:09 -0500

    Corrigiendo header y footer

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { Header } from './shared/header/header';
-import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, FormsModule, Header, Footer],
+  imports: [RouterModule, FormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
