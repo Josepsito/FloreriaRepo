6294d59fc6601126c731a6e5e5a4ce1fcd4dd3b4
AndreaMelisa <U22300170@utp.edu.pe>
2025-11-19 01:52:34 -0500

    Formulario para el contacto

--- frontend/src/app/contacto/contacto.ts
+++ frontend/src/app/contacto/contacto.ts
@@ -11,4 +11,26 @@ import { Footer } from '../shared/footer/footer';
   templateUrl: './contacto.html',
   styleUrls: ['./contacto.css']
 })
-export class Contacto {}
+export class Contacto {
+
+  enviarWhatsApp(event: Event): void {
+    event.preventDefault();
+
+    const nombre = (document.getElementById('nombre') as HTMLInputElement)?.value;
+    const email = (document.getElementById('email') as HTMLInputElement)?.value;
+    const asunto = (document.getElementById('asunto') as HTMLInputElement)?.value;
+    const mensaje = (document.getElementById('mensaje') as HTMLTextAreaElement)?.value;
+
+    const telefonoDestino = "51962739321"; //
+
+    const texto = Hola, soy ${nombre}.
+    Correo: ${email}
+    Asunto: ${asunto}
+    Mensaje: ${mensaje};
+
+    const url = `https://wa.me/${telefonoDestino}?text=${encodeURIComponent(texto)}`;
+
+    window.open(url, '_blank');
+  }
+
+}
